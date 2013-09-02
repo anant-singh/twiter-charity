d946b9c65027609add97fecd0d164346110d436f
Akshay Singh <akshay.x666@gmail.com>
2013-09-02 17:03:15 +0530

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+/**
+ * Routes
+ */
+
+module.exports = function(app) {
+
+    app.get('/', function(req, res) {
+        res.locals = {
+            title: 'Node Boiler'
+        };
+        res.render('index', {
+            partials: {
+                title: 'title'
+            }
+        });
+    });
+
+    /**
+     * Errors
+     */
+
+    app.get('/404', function(req, res, next) {
+        next();
+    });
+
+    app.get('/403', function(req, res, next) {
+        var err = new Error('Not Allowed!');
+        err.status = 403;
+        next(err);
+    });
+
+    app.get('/500', function(req, res, next) {
+        next(new Error());
+    });
+
+}
