0c63ec2ddec89c816f60e221833079f04c65b450
Akshay kr Singh <akshay.x666@gmail.com>
2013-09-22 17:46:00 +0530

    added request accept/reject feature, admin control panel. bumped v0.9.6

--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,7 @@
 var db          = require('../lib/db'),
     moment      = require('moment'),
     validate    = require('validator').check,
+    settings    = require('../settings/config').settings;
     twitter     = require('../lib/twitter');
 
 module.exports = function(app, passportConn) {
@@ -17,7 +18,12 @@ module.exports = function(app, passportConn) {
             next();
     };
 
-    var status = {};
+    var isadmin = function(req, res, next) {
+        if (req.user.username != settings.username)
+            res.redirect('/');
+        else
+            next();
+    }
 
     //==================================================================
     // routes
@@ -25,26 +31,64 @@ module.exports = function(app, passportConn) {
         res.render('index');
     });
 
-    app.get('/auth/twitter/callback', passportConn.passport.authenticate('twitter', {
-        successRedirect: '/#/stage',
-        failureRedirect: '/'
-    }));
-
-    app.get('/tweet/info', auth, function(req, res) {
-        res.send(req.user);
+    app.get('/logout', function(req, res){
+        res.render('logout');
     });
 
-    app.get('/check/reg', auth, function(req, res){
+    app.get('/auth/twitter/callback', function(req, res, next) {
+        passportConn.passport.authenticate('twitter', function(err, user, info) {
+            if (err) {return next(err);}
+            if (!user) {return res.redirect('/');}
+            req.logIn(user, function(err) {
+                if (err) {return next(err); }
+                console.log(user.username + settings.username);
+                if (user.username == settings.username) {return res.redirect('/#/admin');}
+                db.CharityAccount
+                    .findOne({ 'userName': user.username })
+                    .exec(function(err, record) {
+                        if (err) {return next(err);}
+                        if (!record) {return res.redirect('/#/register')}
+                        return res.redirect('/#/denied');
+                    })
+            });
+        })(req, res, next);
+    });
+
+    app.get('/info/isadmin', auth, isadmin, function(req, res) {
+        res.send({flag: true});
+    });
+
+    app.get('/info/admin/getlist', auth, isadmin, function(req, res) {
         db.CharityAccount
-            .findOne({ 'userName': req.user.username })
+            .find({'__v': 0})
             .exec(function(err, record) {
-                if (err) {return res.send(err);}
-                else if (record) {return res.send(record);}
-                else {return res.send(404);}
+                if (err) {res.send(err);}
+                console.log(record);
+                res.send(record);
             })
     });
 
-    app.get('/tweet/timeline', auth, function(req, res) {
+    app.get('/info/user', auth, function(req, res) {
+        res.send(req.user);
+    });
+
+    app.get('/auth/approved', auth, function(req, res) {
+        db.CharityAccount
+            .findOne({'userName': req.user.username})
+            .exec(function(err, record) {
+                if (err) {res.send(err);}
+                res.send(record);
+            })
+    });
+
+    app.get('/info/public/timeline', function(req, res) {
+        twitter.getTimeline(function(err, data) {
+            if (err) res.send(404);
+            res.send(data);
+        })
+    })
+
+    app.get('/info/private/timeline', auth, function(req, res) {
         twitter.getTimeline(function(err, data) {
             if (err) res.send(404);
             res.send(data);
@@ -66,6 +110,20 @@ module.exports = function(app, passportConn) {
                 return res.send({type: 'danger', msg: err});
             }
             return res.send({type: 'success', msg: 'Your account has been registered'});
+        });
+    })
+
+    app.get('/approve/:user', auth, isadmin, function(req, res) {
+        db.CharityAccount.update({ 'userName': req.params.user }, { 'approved': 'true' }, {multi: false}, function(err, record) {
+            if (err) return res.send({type: 'danger', msg: err});
+            return res.send({type: 'success', msg: req.params.user+'\'s request has been approved'});
+        })
+    })
+
+    app.get('/reject/:user', auth, isadmin, function(req, res) {
+        db.CharityAccount.remove({ 'userName': req.params.user }, function(err) {
+            if (err) return res.send({type: 'danger', msg: err});
+            return res.send({type: 'success', msg: req.params.user+ '\'s Request has been rejected'});
         });
     })
     //==================================================================
--- static/angular/app.js
+++ static/angular/app.js
@@ -71,6 +71,20 @@ var app = angular.module('ctApp', ['ui.bootstrap'])
                 templateUrl: 'views/default.html',
                 controller: 'DefaultCtrl'
             })
+            .when('/denied', {
+                templateUrl: 'views/denied.html',
+                controller: 'DeniedCtrl',
+                resolve: {
+                    loggedin: checkLoggedin
+                }
+            })
+            .when('/admin', {
+                templateUrl: 'views/admin.html',
+                controller: 'AdminCtrl',
+                resolve: {
+                    loggedin: checkLoggedin
+                }
+            })
             .when('/tweet', {
                 templateUrl: 'views/tweet.html',
                 controller: 'TweetCtrl',
@@ -114,7 +128,7 @@ var app = angular.module('ctApp', ['ui.bootstrap'])
 
 /**
  * **********************************************************************
- * *************************** Controllers ******************************
+ *                              Controllers
  * **********************************************************************
  */
 /**********************************************************************
@@ -122,39 +136,56 @@ var app = angular.module('ctApp', ['ui.bootstrap'])
 **********************************************************************/
 app.controller('DefaultCtrl', function($scope, $http) {
     $scope.timeline = {};
-    $scope.user = {};
-    $http.get('/tweet/info').success(function(user) {
-        $scope.user = user;
-    })
-    $http.get('/tweet/timeline')
+    $http.get('/info/public/timeline')
         .success(function(data) {
             $scope.timeline.tweets = data;
         });
 });
 
-
+/**********************************************************************
+ * Denied controller
+ **********************************************************************/
+app.controller('DeniedCtrl', function($scope, $http, $location) {
+    $http.
+        get('/auth/approved')
+        .success(function(data){
+            $scope.data = data;
+            if($scope.data.approved == 'true') {
+                $location.url('/tweet');
+            }
+        })
+});
 
 /**********************************************************************
  * Tweet controller
  **********************************************************************/
-app.controller('TweetCtrl', function($scope, $http) {
+app.controller('TweetCtrl', function($scope, $http, $location) {
     $scope.dataStream = {};
     $scope.timeline = {};
     $scope.alerts = [];
     $scope.user = {};
 
-    $http.get('/tweet/info').success(function(user) {
-        $scope.user = user;
-    })
+    $http.
+        get('/info/user')
+        .success(function(data){
+            $scope.user = data;
+        })
+
+    /* Tweet Timeline Start */
+    $http.
+        get('/info/private/timeline')
+        .success(function(data) {
+            $scope.timeline.tweets = data;
+            $scope.term = '@' + $scope.user.username;
+        });
+    /* Tweet Timeline End */
 
     $scope.closeAlert = function(index) {
         $scope.alerts.splice(index, 1);
     };
-
     $scope.encode = function(url) {
         return encodeURI(url);
-    }
-
+    };
     $scope.submit = function() {
         if ($scope.dataStream.tweet.length > 140) {
             $scope.alerts.push({type: 'danger', msg: 'Tweet Length should not exceed 140 characters'});
@@ -167,27 +198,7 @@ app.controller('TweetCtrl', function($scope, $http) {
             .success(function (status) {
                 $scope.alerts.push(status);
             });
-    }
-
-    /**
-     * ********************************************************
-     * Tweet Timeline Start
-     * ********************************************************
-     */
-
-    $http.
-        get('/tweet/timeline')
-        .success(function(data) {
-            $scope.timeline.tweets = data;
-            $scope.term = '@' + $scope.user.username;
-        });
-
-    /**
-     * ********************************************************
-     * Tweet Timeline End
-     * ********************************************************
-     */
-
+    };
 
     /**
      * DataPicker Function
@@ -196,28 +207,23 @@ app.controller('TweetCtrl', function($scope, $http) {
         $scope.dataStream.dateToTweet = new Date();
     };
     $scope.today();
-
     $scope.showWeeks = true;
     $scope.toggleWeeks = function () {
         $scope.showWeeks = ! $scope.showWeeks;
     };
-
     // Disable weekend selection
     $scope.disabled = function(date, mode) {
         return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
     };
-
     $scope.toggleMin = function() {
         $scope.minDate = ( $scope.minDate ) ? null : new Date();
     };
     $scope.toggleMin();
-
     $scope.open = function() {
         $timeout(function() {
             $scope.opened = true;
         });
     };
-
     $scope.dateOptions = {
         'year-format': "'yy'",
         'starting-day': 1
@@ -227,41 +233,21 @@ app.controller('TweetCtrl', function($scope, $http) {
      * TimePicker Controller
      */
     $scope.dataStream.timeToTweet = new Date();
-
     $scope.hstep = 1;
     $scope.mstep = 5;
-
     $scope.ismeridian = true;
     $scope.toggleMode = function() {
         $scope.ismeridian = ! $scope.ismeridian;
     };
-
     $scope.update = function() {
         var d = new Date();
         d.setHours( 14 );
         d.setMinutes( 0 );
         $scope.dataStream.timeToTweet = d;
     };
-
     $scope.changed = function () {
         console.log('Time changed to: ' + $scope.dataStream.timeToTweet);
     };
-});
-
-/**********************************************************************
- * Stage controller
- **********************************************************************/
-app.controller('StageCtrl', function($scope, $rootScope, $http, $location) {
-    $http.get('/tweet/info').success(function(user) {
-        $scope.user = user;
-    })
-    $http.get('/check/reg')
-        .success(function(res) {
-            $location.url('/tweet');
-        })
-        .error(function() {
-            $location.url('/register');
-        })
 });
 
 /**********************************************************************
@@ -279,7 +265,51 @@ app.controller('RegisterCtrl', function($scope, $rootScope, $http, $location) {
         $http.
             post('/account/data', $scope.account)
             .success(function(status) {
-                $location.url('/tweet');
+                $location.url('/denied');
+            })
+    }
+});
+
+/**********************************************************************
+ * Admin controller
+ **********************************************************************/
+app.controller('AdminCtrl', function($scope, $rootScope, $http, $location) {
+    $scope.alerts = [];
+    $scope.records = {};
+    $http.get('/info/isadmin')
+        .success(function(flag) {
+            $scope.flag = flag;
+            if ($scope.flag.flag == true) {
+                $rootScope.admin = true;
+            } else {
+                $rootScope.admin = false;
+                $location.url('/denied');
+            }
+        })
+
+    $http.get('/info/admin/getlist')
+        .success(function(data) {
+            $scope.records = data;
+            if ($scope.records.length < 1) $scope.alerts.push({type: 'success', msg: 'You do not have any pending requests'});
+        })
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+
+    $scope.approve = function(index, userName) {
+        $http.get('/approve/'+userName)
+            .success(function(data) {
+                $scope.alerts.push(data);
+                $scope.records.splice(index, 1);
+            })
+    }
+
+    $scope.reject = function(index, userName) {
+        $http.get('/reject/'+userName)
+            .success(function(data) {
+                $scope.alerts.push(data);
+                $scope.records.splice(index, 1);
             })
     }
 });
