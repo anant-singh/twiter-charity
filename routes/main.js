7f8395ef22c36fcc1c6f8b84c9dfd5d474ca1ccf
Akshay kr Singh <akshay.x666@gmail.com>
2013-09-24 17:18:17 +0530

    bug fixes, UI update on account approval

--- routes/main.js
+++ routes/main.js
@@ -61,6 +61,7 @@ module.exports = function(app, passportConn) {
     app.get('/info/admin/getlist', auth, isadmin, function(req, res) {
         db.CharityAccount
             .find({'__v': 0})
+            .where({'approved': 'false'})
             .exec(function(err, record) {
                 if (err) {res.send(err);}
                 console.log(record);
